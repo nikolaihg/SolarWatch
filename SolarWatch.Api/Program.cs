d7b362809a2835879c5798e1cda0a4c91567867e
Nikolai Gangstø <nikolai.gangstoe@gmail.com>
2026-01-27 10:53:57 +0100

    added userdbcontext

--- SolarWatch.Api/Program.cs
+++ SolarWatch.Api/Program.cs
@@ -13,8 +13,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 string jwtIssuer = Environment.GetEnvironmentVariable("JWT_VALID_ISSUER")!;
 string jwtAudience = Environment.GetEnvironmentVariable("JWT_VALID_AUDIENCE")!;
-string jwtExpires = Environment.GetEnvironmentVariable("JWT_EXPIRES_IN_MINUTES")!;
 string jwtKey = Environment.GetEnvironmentVariable("JWT_SIGNING_KEY")!;
+
+var host = builder.Configuration["DB_HOST"] ?? throw new ArgumentException("DB_HOST is required");
+var port = builder.Configuration["DB_PORT"] ?? throw new ArgumentException("DB_PORT is required");
+var database = builder.Configuration["DB_NAME"] ?? throw new ArgumentException("DB_NAME is required");
+var username = builder.Configuration["DB_USER"] ?? throw new ArgumentException("DB_USER is required");
+var password = builder.Configuration["DB_PASSWORD"] ?? throw new ArgumentException("DB_PASSWORD is required");
+
+var connectionString = $"Host={host};Port={port};Username={username};Password={password};Database={database}";
 
 builder.Services.AddAuthentication(options =>
 {
@@ -36,13 +43,10 @@ builder.Services.AddAuthorization();
 
 builder.Services.AddDbContext<SolarWatchDbContext>(options =>
 {
-    var host = builder.Configuration["DB_HOST"] ?? throw new ArgumentException("DB_HOST is required");
-    var port = builder.Configuration["DB_PORT"] ?? throw new ArgumentException("DB_PORT is required");
-    var database = builder.Configuration["DB_NAME"] ?? throw new ArgumentException("DB_NAME is required");
-    var username = builder.Configuration["DB_USER"] ?? throw new ArgumentException("DB_USER is required");
-    var password = builder.Configuration["DB_PASSWORD"] ?? throw new ArgumentException("DB_PASSWORD is required");
-
-    var connectionString = $"Host={host};Port={port};Username={username};Password={password};Database={database}";
+    options.UseNpgsql(connectionString);
+});
+builder.Services.AddDbContext<UserDbContext>(options =>
+{
     options.UseNpgsql(connectionString);
 });
 
@@ -52,6 +56,7 @@ builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddHttpClient();
+
 builder.Services.AddScoped<ISunriseSunsetService, SunriseSunsetService>();
 builder.Services.AddScoped<ILocationService, LocationService>();
 builder.Services.AddScoped<ICityRepository, CityRepository>();
