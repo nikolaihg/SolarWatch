5b14f7b151b6a2e60bfef06b915b2fe6e68bb7df
Nikolai Gangstø <nikolai.gangstoe@gmail.com>
2026-02-20 11:21:18 +0100

    working frontend docker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         '/api': {
-          target: env.VITE_API_URL || 'http://localhost:5195',
+          target: env.VITE_API_URL || 'http://localhost:8080',
           changeOrigin: true,
           secure: false,
         }
